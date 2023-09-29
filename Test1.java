0c404b4ab082374f2677741fc497e94bf67ad76e
ivandavid858 <ivandavid858@gmail.com>
2023-09-29 20:19:14 +0200

    Archivos tarea01

---
+++ Test1.java
@@ -0,0 +1,15 @@
+public class Test1 {
+	public static void main(String[] args) {
+		Integer num1 = 125;
+		Integer num2 = 20;
+		
+		int n1 = num1.intValue();
+		int n2 = num2.intValue();
+		System.out.println("Suma de "+n1+" y "+n2+" vale " + (n1+n2));
+		if(n1+n2 > 30)
+			n1++;
+		else
+			n1--;
+		System.out.println(n1);
+	}
+}
